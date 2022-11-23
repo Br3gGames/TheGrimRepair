8ccaf1beb6af4b8244bf871db08b780d8a245b06
Br3g <118094144+Br3gGames@users.noreply.github.com>
2022-11-22 18:53:34 -0700

    Add Portuguese

---
+++ Locales/Locale-ptBR.lua
@@ -0,0 +1,22 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("TGR", "ptBR")
+
+if L then
+    L["AUTO_REPAIR_HEADER"] = "Auto Reparação"
+    L["AUTO_SELL_HEADER"] = "Venda Automática"
+    L["KEEP_GRAY_DESC"] = "Mantenha armadura cinza e armas para transmog"
+    L["KEEP_GRAY_HEADER"] = "Mantenha itens transmog (armaduras e armas)"
+    L["MSG_GUILD_REPAIR_FAIL"] = "Você não está em uma guilda ou não tem permissão para usar os reparos da guilda"
+    L["MSG_GUILD_REPAIR_SUCCESS"] = "Guilda reparou você por"
+    L["MSG_NO_MONEY"] = "Você não pode consertar porque não tem dinheiro"
+    L["MSG_REPAIRED"] = "Reparado para"
+    L["MSG_SKIPPED"] = "Pular reparo/venda automática!"
+    L["MSG_SOLD"] = "Vendido"
+    L["MSG_TIP_DISABLE"] = "Dica: Segurar \"Shift\" enquanto clica em um fornecedor desativará a execução deste complemento apenas para essa interação"
+    L["SALE_DETAILS_DESC"] = "Mostrar todos os itens vendidos automaticamente e o valor de venda do fornecedor"
+    L["SALE_DETAILS_HEADER"] = "Mostrar detalhes da venda"
+    L["SELL_GRAY_DESC"] = "Vender todos os itens cinza"
+    L["SELL_GRAY_HEADER"] = "Vender itens cinza"
+    L["SELLING_RESTRICTIONS_HEADER"] = "Restrições de venda"
+    L["USE_GUILD_REPAIRS_DESC"] = "Use reparos de guilda (se disponível)"
+    L["USE_GUILD_REPAIRS_HEADER"] = "Usar reparos de guilda"
+end
